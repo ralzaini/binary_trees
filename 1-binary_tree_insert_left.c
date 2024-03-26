69aebe1e944d6e09aec14746ee527e3044192fc9
ralzaini <rehabalzaini@gmail.com>
2024-03-26 06:03:12 -0700

    ww

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,33 +10,33 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *left_node, *tmp;
+	binary_tree_t *left_node, *tmp;
 
-    if (!parent)
-        return (NULL);
+	if (!parent)
+		return (NULL);
 
-    left_node = malloc(sizeof(binary_tree_t));
+	left_node = malloc(sizeof(binary_tree_t));
 
-    if (!left_node)
-        return (NULL);
+	if (!left_node)
+		return (NULL);
 
-    left_node->n = value;
-    left_node->left = NULL;
-    left_node->right = NULL;
+	left_node->n = value;
+	left_node->left = NULL;
+	left_node->right = NULL;
 
-    if (!parent->left)
-    {
-        left_node->parent = parent;
-        parent->left = left_node;
-    }
-    else
-    {
-        tmp = parent->left;
-        parent->left = left_node;
-        parent->left->left = tmp;
-        parent->left->parent = parent;
-        parent->left->left->parent = parent->left;
-    }
+	if (!parent->left)
+	{
+		left_node->parent = parent;
+		parent->left = left_node;
+	}
+	else
+	{
+		tmp = parent->left;
+		parent->left = left_node;
+		parent->left->left = tmp;
+		parent->left->parent = parent;
+		parent->left->left->parent = parent->left;
+	}
 
-    return (left_node);
+	return (left_node);
 }
