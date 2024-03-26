301a366a5f50503583c4cfa02ff4784cec605221
ralzaini <rehabalzaini@gmail.com>
2024-03-26 15:26:31 +0300

    task1

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node of the node to create.
+ * @value: Integer value of the new node
+ *
+ * Return: Pointer to the newly created node or NULL if it fails.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+
+    new_node = malloc(sizeof(binary_tree_t));
+    if (!new_node)
+        return (NULL);
+
+    new_node->n = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    new_node->parent = parent;
+
+    return (new_node);
+}
---
+++ binary_trees.h
@@ -0,0 +1,60 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+/* Binary Search Tree */
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
+
+/**
+* struct queue_node - for storing nodes in a queue
+*
+* @node: the node being stored
+* @next: pointer to the next node in the queue
+*/
+typedef struct queue_node
+{
+	binary_tree_t *node;
+	struct queue_node *next;
+} queue_node_t;
+
+/**
+* struct queue - struct datastrucutre for queue
+*
+* @front: pointer to the front of the queue
+* @rear: pointer to the rear of the queue
+*/
+typedef struct queue
+{
+	queue_node_t *front;
+	queue_node_t *rear;
+} queue_t;
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#endif
