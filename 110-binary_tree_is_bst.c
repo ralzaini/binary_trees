1d4332dc88337400c7203f1776510e3564ac7b18
ralzaini <rehabalzaini@gmail.com>
2024-03-27 13:06:51 -0700

    wwqq

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -6,7 +6,7 @@
  * @min: minimum range of the tree node values
  * @max: maximum value of the tree node values
  * Return: returns 1 if it is a binary search tree, 0 otherwise.
-*/
+ */
 int tree_is_best(const binary_tree_t *tree, int minimum, int maximum)
 {
 	if (!tree)
@@ -16,14 +16,14 @@ int tree_is_best(const binary_tree_t *tree, int minimum, int maximum)
 		return (0);
 
 	return (tree_is_best(tree->left, minimum, tree->n)
-	&& tree_is_best(tree->right, tree->n, maximum));
+			&& tree_is_best(tree->right, tree->n, maximum));
 }
 
 /**
-* binary_tree_is_bst - checks if a binary tree is a BST
-* @tree: binary tree to be checked
-* Return: 1 if tree is BST, 0 otherwise
-*/
+ * binary_tree_is_bst - checks if a binary tree is a BST
+ * @tree: binary tree to be checked
+ * Return: 1 if tree is BST, 0 otherwise
+ */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (!tree)
