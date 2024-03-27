a14697b1c06fc439772d896a3008a980839c485a
ralzaini <rehabalzaini@gmail.com>
2024-03-27 23:05:22 +0300

    sssw

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * tree_is_bst - helper function to check left and right subtree for BST
+ * @tree: pointer to the tree to be checked
+ * @min: minimum range of the tree node values
+ * @max: maximum value of the tree node values
+ * Return: returns 1 if it is a binary search tree, 0 otherwise.
+*/
+int tree_is_best(const binary_tree_t *tree, int minimum, int maximum)
+{
+	if (!tree)
+		return (1);
+
+	if (tree->n <= minimum || tree->n >= maximum)
+		return (0);
+
+	return (tree_is_best(tree->left, minimum, tree->n)
+	&& tree_is_best(tree->right, tree->n, maximum));
+}
+
+/**
+* binary_tree_is_bst - checks if a binary tree is a BST
+* @tree: binary tree to be checked
+* Return: 1 if tree is BST, 0 otherwise
+*/
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+
+	return (tree_is_best(tree, INT_MIN, INT_MAX));
+}
--- binary_trees.h
+++ binary_trees.h
@@ -82,5 +82,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+int tree_is_best(const binary_tree_t *tree, int min, int max);
 
 #endif
