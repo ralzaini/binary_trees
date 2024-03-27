a165c4271808d23b603a66e3ce20344d65ca1b83
ralzaini <rehabalzaini@gmail.com>
2024-03-27 13:35:23 +0300

    iilo

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: measures the balance factor of a binary tree
+ * Return: the balance factor of a the binary tree, 0 if tree is NULL
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+
+    if ((tree->left && !tree->right) || (!tree->left && tree->right))
+        return (0);
+
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+
+    return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+}
